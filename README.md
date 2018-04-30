9cafb102180aa73dccb2bae7288c8c337e3139bd
DESKTOP-5947V8N\Gayan <gayan.sandamal@outlook.com>
2018-04-30 22:40:35 +0530

    Initialization

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# php-image-resizer
-A simple mathematical approach to resize images(php 5.3+)
+# php-image-resizer with aspect ratio
+A simple mathematical approach to resize images (php 5.3 or above)
+
+1. Download "php-image-resizer.php" and copy it into your image directory.
+    example 1 - uploads/php-image-resizer.php
+    example 2 - images/php-image-resizer.php
+
+2. Give the image file name that you want to resize.
+    Line #3
+    Step 1. if your image file name is "sample-image-jpg".
+    Step 2. $source_url = "sample-image.jpg";
+
+    Or you can execute this php file once the image is uploaded.
+
+    Step 1. Use a any query to get the image file name like in below
+<?php
+    include_once '../conn.php';
+    $get_last_img = "
+    SELECT
+    REPLACE(img_path, '/uploads/', '') AS img_path
+    FROM image
+    ORDER BY img_id DESC
+    LIMIT 1
+    ";
+
+    $result_last_img_path = $conn->query($get_last_img_path);
+    if ($result_last_im->num_rows > 0) {
+        while ($row = $result_last_img->fetch_assoc()) {
+            $img_path = $row["img_path"];
+            }
+            echo $img_path . "image is available to resize";
+    } else {
+        echo "Image is not found";
+    }
+?>
+
+    Step 2. Assign image name variable to $source_url variable in line #3
+    example -
+    $source_url = $img_path;
+
+3. Define an image quality. The quality range is from 1 to 100. check line #11. 
+    example - $quality = 10;
+
+4. Define the width that you want as the final output of the image. Line #19.
+    example - $after_width = 250;
+    * No need to specify a unit such as px(Pixels). It will automatically get the value in pixels.
+
+5. It will check the width of input image is larger than the defined output width and will do the for you.
+
+    ENJOY! ;-)
---
+++ php-image-resizer.php
@@ -0,0 +1,63 @@
+<?php
+//path for the image
+$source_url = "sample-image.jpg";
+
+//separate the file name and the extention
+$source_url_parts = pathinfo($source_url);
+$filename = $source_url_parts['filename'];
+$extension = $source_url_parts['extension'];
+
+//define the quality from 1 to 100
+$quality = 10;
+
+//detect the width and the height of original image
+list($width, $height) = getimagesize($source_url);
+$width;
+$height;
+
+//define any width that you want as the output. mine is 200px.
+$after_width = 250;
+
+//resize only when the original image is larger than expected with.
+//this helps you to avoid from unwanted resizing.
+if ($width > $after_width) {
+    
+    //get the reduced width
+    $reduced_width = ($width - $after_width);
+    //now convert the reduced width to a percentage and round it to 2 decimal places
+    $reduced_radio = round(($reduced_width / $width) * 100, 2);
+    
+    //ALL GOOD! let's reduce the same percentage from the height and round it to 2 decimal places
+    $reduced_height = round(($height / 100) * $reduced_radio, 2);
+    //reduce the calculated height from the original height
+    $after_height = $height - $reduced_height;
+    
+    //Now detect the file extension
+    //if the file extension is 'jpg', 'jpeg', 'JPG' or 'JPEG'
+    if ($extension == 'jpg' || $extension == 'jpeg' || $extension == 'JPG' || $extension == 'JPEG') {
+        //then return the image as a jpeg image for the next step
+        $img = imagecreatefromjpeg($source_url);
+    } elseif ($extension == 'png' || $extension == 'PNG') {
+        //then return the image as a png image for the next step
+        $img = imagecreatefrompng($source_url);
+    } else {
+        //show an error message if the file extension is not available
+        echo 'image extension is not supporting';
+    }
+    
+    //HERE YOU GO :)
+    //Let's do the resize thing
+    //imagescale([returned image], [width of the resized image], [height of the resized image], [quality of the resized image]);
+    $imgResized = imagescale($img, $after_width, $after_height, $quality);
+    
+    //now save the resized image with a suffix called "-resized" and with its extension. 
+//    imagejpeg($imgResized, $filename . '-resized.'.$extension);
+    imagejpeg($imgResized, $filename . '.'.$extension);
+    
+    //Finally frees any memory associated with image
+    //**NOTE THAT THIS WONT DELETE THE IMAGE
+    imagedestroy($img);
+    imagedestroy($imgResized);
+    echo 'successfully resized';
+}
+?>
